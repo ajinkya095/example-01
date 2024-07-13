999a1b1a3411cd791212ce55b172dc6399ff9b50
ajinkya095 <ajinkyanagture0102@gmail.com>
2024-07-14 01:12:42 +0530

    update-01

---
+++ src/Bank_Application/main.java
@@ -0,0 +1,13 @@
+package Bank_Application;
+
+import java.util.Scanner;
+
+public class main {
+	
+	public static void main(String[] args) 
+	{
+		Operation operlation = new Operation();
+		operlation.bankinfo();
+	}
+
+}
